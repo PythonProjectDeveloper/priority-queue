db18f4df8c333c17eaae4068e7acf6f008a8d6e4
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-03-11 16:59:44 +0300

    feat: implement the insertNode function in the MaxHeap class

--- src/max-heap.js
+++ src/max-heap.js
@@ -8,7 +8,7 @@ class MaxHeap {
 
 	push(data, priority) {
 		let node = new Node(data, priority)
-		
+
 		this.insertNode(node)
 		this.shiftNodeUp(node)
 	}
@@ -38,7 +38,15 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		if (!this.parentNodes.length)
+			this.root = node
+		else 
+			this.parentNodes[0].appendChild(node)
+
+		this.parentNodes.push(node)
+
+		if (this.parentNodes[0].left && this.parentNodes[0].right)
+			this.parentNodes.shift()
 	}
 
 	shiftNodeUp(node) {
