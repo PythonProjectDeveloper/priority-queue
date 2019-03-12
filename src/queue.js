6bc4917319e099d9361f18dca0538c81ad020ae6
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-03-12 18:05:54 +0300

    feat: implement the constructor function in the PriorityQueue class

--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,8 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.heap = new MaxHeap()
+		this.maxSize = maxSize || 30
 	}
 
 	push(data, priority) {
